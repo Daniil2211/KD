bb5b149037902bca69bf5fc4dfe7be3f28c9cbae
Corginatus <70595766+Corginatus@users.noreply.github.com>
2022-01-31 14:29:55 +0300

    запрос получения статистики звонков + логи

--- main.py
+++ main.py
@@ -1,38 +1,17 @@
-import requests
-from datetime import datetime
-import time
-import json
-
-#def send_message():
-#    url = f'https://api.telegram.org/bot{bot_token}/SendMessage?chat_id={chat_id}&parse_mode=Markdown&text="Hello"'
-#    resp = requests.get(url)
-
-tg_api_token = '5184647613:AAEPob6ZN6Pvm6PlOEbIpdYnjnY21635Mqk'
-tg_chat_id = '922463874'
-
-def send_tg_message(text='Cell execution completed.'):
-    requests.post(
-        'https://api.telegram.org/' +
-        'bot{}/sendMessage'.format(tg_api_token),
-        params=dict(chat_id=tg_chat_id, text=text)
-    )
+r = requests.post('https://api-v3.neuro.net/api/v2/ext/auth', auth=("mylaw.api@gmail.com", "9XuXXHbP"))
+response_json = r.json()
 
 url = 'https://cms-v3.neuro.net/api/v2/log/dialog'
-r = requests.post('https://api-v3.neuro.net/api/v2/ext/auth', auth=("mylaw.api@gmail.com", "9XuXXHbP"))
-data_auth = r.json()
-refresh_token = data_auth["refresh_token"]
-
-nn.log("")
-header = {'Authorization': "Bearer " + refresh_token,
+header = {'Authorization': "Bearer " + response_json['refresh_token'],
           'Content-Type': 'application/json'}
 param_tuples = {'limit': 100,
                 'offset': 0,
                 'where': {
                     'agent_uuid': "03ec4ccc-a775-4395-9abe-49cefbfac9fc",
-                    'call_uuid': [],
-                    'dialog_uuid': [],
+                    'call_uuid': [nn.env("call_uuid")],
+                    'dialog_uuid': [nn.env("dialog_uuid")],
                     'end_date': "30-01-2022 13:14:58",
-                    'msisdn': ['9313689670'],
+                    'msisdn': [nn.dialog['msisdn']],
                     'start_date': "23-01-2022 13:14:58"}}
 rr = requests.post(url, headers=header, json=param_tuples)
 rr.json()
@@ -45,4 +24,6 @@ nn.log("TEXT: ", rr.text)
 nn.log("CONTENT: ", rr.content)
 nn.log("JSON: ", rr.json)
 
-
+nn.log("Start request: ", str(datetime.datetime.now()))
+nn.log("total_count: ", rr.json()['total_count'])
+nn.log("Finish request: ", str(datetime.datetime.now()))
